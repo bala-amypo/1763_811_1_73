150528e07e3f3a1330bddd28bd6fed8b5ee9ff95
bala-amypo <bala@amypo.in>
2025-12-18 10:00:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ApprovalActionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ApprovalActionServiceImpl.java
@@ -1,1 +1,19 @@
 package com.example.demo.service.impl;
+
+import com.example.demo.repository.*;
+import com.example.demo.entity.ApprovalAction;
+import com.example.demo.service.ApprovalActionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApprovalActionServiceImpl implements ApprovalActionService {
+
+    public ApprovalActionServiceImpl(
+            ApprovalActionRepository actionRepo,
+            ApprovalRequestRepository requestRepo) {
+    }
+
+    public ApprovalAction recordAction(ApprovalAction action) {
+        return action;
+    }
+}
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,1 +1,25 @@
 package com.example.demo.service.impl;
+
+import com.example.demo.repository.*;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    public UserServiceImpl(
+            UserRepository userRepo,
+            RoleRepository roleRepo,
+            PasswordEncoder encoder) {
+    }
+
+    public User registerUser(User user, String roleName) {
+        return user;
+    }
+
+    public User findByUsername(String username) {
+        return null;
+    }
+}
