94472720459fe2c4d89b0eb80cb267720a49c3ef
bala-amypo <bala@amypo.in>
2025-12-19 09:36:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,33 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-
-        SecurityScheme securityScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Multi-Level Approval Workflow Engine")
-                        .description("Spring Boot REST API with JWT Security")
-                        .version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
-                .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes("Authorization", securityScheme)
-                );
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9217.408procr.amypo.ai/")
+                ));
+        }
 }
