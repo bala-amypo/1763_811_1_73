11c22267b9c1cfea42134f0c34123e46d83a94a1
bala-amypo <bala@amypo.in>
2025-12-26 06:33:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class AuthResponse {
+
+    private String token;
+    private String username;
+    private List<String> roles;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
